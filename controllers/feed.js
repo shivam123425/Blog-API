096f3e3d038b36c6ac35433e0001be8270cb144a
shivam123425 <shivam123425@gmail.com>
2019-03-12 15:53:25 +0530

    Add validations

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,8 +1,10 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
-        _id: '1',
+        _id: "1",
         title: "First Post",
         content: "This is the first post!",
         imageUrl: "images/book.jpg",
@@ -16,11 +18,24 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect",
+      errors: errors.array()
+    });
+  }
   const title = req.body.title;
   const content = req.body.content;
   // Create post in db
   res.status(201).json({
     message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title: title, content: content }
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Shivam" },
+      createdAt: new Date()
+    }
   });
 };
