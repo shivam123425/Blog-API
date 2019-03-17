6e6b88e9eeaec89f4b669ad454a0f78d89d9e5aa
shivam123425 <shivam123425@gmail.com>
2019-03-17 17:35:43 +0530

    Add multer to upload and save files

--- app.js
+++ app.js
@@ -2,17 +2,39 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
+const multer = require("multer");
 const feedRoutes = require("./routes/feed");
 
 const keys = require("./keys/keys");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 const PORT = process.env.PORT || 8080;
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
-
+app.use(multer({ storage: fileStorage, fileFilter }).single("image"));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
