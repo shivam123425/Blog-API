6b40fd85a8afef33d8d4869466c877266eb5e7b1
shivam123425 <shivam123425@gmail.com>
2019-03-17 16:52:22 +0530

    Add /post/:postId route and logib

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,20 +2,19 @@ const { validationResult } = require("express-validator/check");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/book.jpg",
-        creator: {
-          name: "Shivam"
-        },
-        createdAt: new Date()
+  Post.find()
+    .then(posts => {
+      res.status(200).json({
+        message: "Fetched posts successfully",
+        posts
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
       }
-    ]
-  });
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
@@ -40,6 +39,28 @@ exports.createPost = (req, res, next) => {
       console.log(post);
       res.status(201).json({
         message: "Post created successfully!",
+        post
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error("Could not find the post.");
+        error.statusCode = 404;
+        throw error; // throw to the catch block which uses next()
+      }
+      res.status(200).json({
+        message: "Post fetched",
         post
       });
     })
