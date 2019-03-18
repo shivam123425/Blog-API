ce12a82d7732b18d61decab374403cfc99a236d6
shivam123425 <shivam123425@gmail.com>
2019-03-19 00:59:01 +0530

    Connect User and Post

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,6 +2,7 @@ const { validationResult } = require("express-validator/check");
 const fs = require("fs");
 const path = require("path");
 const Post = require("../models/post");
+const User = require("../models/user");
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -45,20 +46,30 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   const imageUrl = req.file.path;
+
+  let creator;
   // Create post in db
   const post = new Post({
     title,
     content,
     imageUrl,
-    creator: { name: "Shivam" }
+    creator: req.userId
   });
   post
     .save()
-    .then(post => {
-      console.log(post);
+    .then(result => {
+      return User.findById(req.userId);
+    })
+    .then(user => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then(result => {
       res.status(201).json({
         message: "Post created successfully!",
-        post
+        post,
+        creator: { _id: creator._id, name: creator.name }
       });
     })
     .catch(err => {
